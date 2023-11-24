35f04f9b06f8797d0fe13607bd1f1b10106ed186
laozf@addcn.com <laozf@addcn.com>
2023-11-24 15:54:47 +0800

    1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,27 @@
+import {useEffect} from 'react'
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const getData = async ()=>{
+      const res =await axios({
+        method: 'get',
+        url: 'http://localhost:8080/api/getNum',
+      });
+      console.log(res.data,'..res')
+  }
+
+  useEffect(()=>{
+    getData()
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world!
         </p>
         <a
           className="App-link"
